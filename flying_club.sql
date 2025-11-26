e9b0dc41466d12120f9d769c82d935b0481697d2
zhen4373 <anthonykwowaichun@icloud.com>
2025-11-26 16:00:22 +0000

    Update the flying_club.sql file to correct the table structure and add necessary columns.

--- flying_club.sql
+++ flying_club.sql
@@ -1,96 +1,69 @@
-create table pilot (
-   pilot_id int ,
-   first_name varchar(50),
-   last_name varchar(50),
-   age int,
-   gender varchar(10),
-   phone varchar(15),
-   email varchar(50),
-   types_of_pilot varchar(50)
-   '(Captain,Chief Officer, and Officer)'   
-   Max_flights_per_day boolean default true,
-   primary key (flight_id),
+create database flying_club;
 
+create table pilot (
+   pilot_id int, not null,
+   first_name varchar(50),not null,
+   last_name varchar(50),not null,
+   age int, not null,
+   gender varchar(10), not null,
+   phone varchar(15), not null,
+   email varchar(50), not null,
+   types_of_pilot varchar(50), not null
+   '(Captain,Chief Officer, and Officer)'   
+   Max_flights_per_day int, not null,
+   'Captain:2,Chief Officer:2,Officer:1'
+   primary key (flight_id),
 
 )
 
 
 create table passengers (
-   passenger_id int,
-   first_name varchar(50),
-   last_name varchar(50),
-   age int,
-   superviser_need boolean default false,
-   gender varchar(10),
-   phone varchar(15),
-   email varchar(50),
+   passenger_id int, not null,
+   first_name varchar(50), not null,
+   last_name varchar(50), not null,
+   age int, not null,
+   under_18 boolean default false,not null,
+   gender varchar(10), not null,
+   phone varchar(15), not null,
+   email varchar(50), not null,
    PRIMARY key (passenger_id)
 )
 
-
-create table instructors(
-   instructor_id int,
-   first_name varchar(50),
-   last_name varchar(50),
-   age int,
-   gender varchar(10),
-   phone varchar(15),
-   email varchar(50),
-   types_of_pilot varchar(50)
-   '(Captain,Chief Officer, and Officer)'
-   primary key (instructor_id)
-   foreign key (types_of_pilot) references pilot(types_of_pilot)
+create table booking (
+   booking_id int, not null,
+   booking_name varchar(50), not null,
+   contact_number int, not null
+   flight_id int, not null,
+   passenger_id int, not null,
+   PRIMARY key(booking_id),
+   foreign key(flight_id) references flights(flight_id),
+   foreign key(passenger_id) references passengers(passenger_id)
 )
 
 
-create table flights(
-   flight_id int ,
-   flight_date DATE,
-   flight_scheduled_Time TIME,
-   flight_duration TIME,
-   Flight_Type varchar(50)
-   '(Training Flight, Pleasure Flight)'
-   flight_status varchar(50)
-   '(Scheduled, Cancelled, Delayed, Completed)'
+create table crew (
+   crew_id int, not null,
+   pilot_id int, not null,
+   role_in_flight varchar(50), not null,
+   PRIMARY key (crew_id)
+   foreign key(pilot_id) references pilot(pilot_id)
+)
+
+
+create table flights (
+   flight_id int, not null,
+   flight_type varchar(50), not null,
+   flight_date date, not null,
+   flight_time time, not null,
+   duration varchar(10), not null,
+   "30min, 60min, 120min, half-day"
+   route varchar(50), not null,
+   status varchar(50), not null,
+   'scheduled, cancelled, completed'
    PRIMARY key (flight_id)
 )
 
 
-create table Pleasure_flights (
-   flight_id int,
-   type_of_pleasure_flight varchar(50)
-   '30 minutes, 60 minutes, 2 hours, half-day'
-   passenger_id_1 int,
-   passenger_id_2 int,
-   passenger_id_3 int,
-   pilot_id_1 int,
-   pilot_id_2 int,
-   foreign key (flight_id) references flights(flight_id),
-   foreign key (passenger_id_1) references passengers(passenger_id),
-   foreign key (passenger_id_2) references passengers(passenger_id),
-   foreign key (passenger_id_3) references passengers(passenger_id)
-   foreign key(pilot_id_1) references pilot(pilot_id),
-   foreign key(pilot_id_2) references pilot(pilot_id),
 
-
-)
-create table Training_flights(
-   flight_id int,
-   instructor_id int,
-   foreign key (flight_id) references flights(flight_id),
-   foreign key (instructor_id) references instructors(instructor_id)
-
-
-)
-
-
-create table Booking(
-   booking_id int,
-   Flight_ID int,
-   passenger_id int,
-   PRIMARY key(booking_id)
-   foreign key(flight_id) references flights(flight_id)
-   foreign key(passenger_id) references passengers(passenger_id)
-)
 
 
