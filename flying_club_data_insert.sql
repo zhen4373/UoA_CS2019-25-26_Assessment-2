57677227f068f391539879b23dfb6b2414dc72d9
zhen4373 <anthonykwowaichun@icloud.com>
2025-11-26 16:57:43 +0000

    copy "customer" from dreamhome3.sql to flying_club_data_insert.sql

---
+++ flying_club_data_insert.sql
@@ -0,0 +1,16 @@
+--will join back to flying_club.sql
+
+
+INSERT INTO 'passenger' ('passenger_id', 'first_name', 'last_name','age','gender','phone', 'email') VALUES 
+('P001', 'Aline', 'Stewart','25','M','0141-848-1825', 'AlineStewart@email.com');
+('P002', 'Mary', 'Tregear','30','F','01224-196720', 'MaryTregear@email.com');
+('P003', 'Mike', 'Ritchie','35','M','01475-392178', 'MikeRitchie@email.com');
+('P004', 'John', 'Kay','40','M','0207-774-5632', 'JohnKay@email.com');
+--data from dreamhome3.sql
+-- ('CR56', 'Aline', 'Stewart', '0141-848-1825', 'Flat', 350),
+-- ('CR62', 'Mary', 'Tregear', '01224-196720', 'Flat', 600),
+-- ('CR74', 'Mike', 'Ritchie', '01475-392178', 'House', 750),
+-- ('CR76', 'John', 'Kay', '0207-774-5632', 'Flat', 425);
+
+
+
